08600ad2e8350e3f322c0b3ba505dd811d1f24d9
Manav Shah <shahmanav1308@gmail.com>
2024-04-01 17:36:05 +0530

    Remove node_modules

--- src/pages/AppBar/AppBar.js
+++ src/pages/AppBar/AppBar.js
@@ -189,8 +189,8 @@ function AppBar() {
                         {[...products, ...callsToAction].map((item) => (
                           <Disclosure.Button
                             key={item.name}
-                            as="a"
-                            href={item.to}
+                            as={Link}
+                            to={item.to}
                             className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                           >
                             {item.name}
